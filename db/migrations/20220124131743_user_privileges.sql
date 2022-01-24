82b52c31b4e551e1e4a4565e31430aa884790486
DarkJediDJ <xintut@mail.ru>
2022-01-24 13:54:17 +0300

    db fix

--- db/migrations/20220124131743_user_privileges.sql
+++ db/migrations/20220124131743_user_privileges.sql
@@ -3,6 +3,8 @@ CREATE TABLE IF NOT EXISTS public.user_privileges
 (
     user_id integer NOT NULL,
     privilege_id integer NOT NULL,
+    user_privilege_id integer NOT NULL DEFAULT nextval('user_privileges_user_privilege_id_seq'::regclass),
+    CONSTRAINT user_privileges_pkey PRIMARY KEY (user_privilege_id),
     CONSTRAINT "FK_user_privileges_to_privileges" FOREIGN KEY (privilege_id)
         REFERENCES public.privileges (privilege_id) MATCH SIMPLE
         ON UPDATE NO ACTION
