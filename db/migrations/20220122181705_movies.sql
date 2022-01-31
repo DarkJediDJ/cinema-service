244bfce49269b2ce2d7665d56fd1aa4b88c7ac0d
DarkJediDJ <xintut@mail.ru>
2022-01-31 17:13:34 +0300

    fix(Halls): migrations

--- db/migrations/20220122181705_movies.sql
+++ db/migrations/20220122181705_movies.sql
@@ -3,9 +3,9 @@ CREATE TABLE IF NOT EXISTS public.movies
 (
     name character varying(50) NOT NULL,
     duration time without time zone NOT NULL,
-    id integer SERIAL,
+    id SERIAL,
     CONSTRAINT movies_pkey PRIMARY KEY (id)
-)
+);
 
 
 -- +goose Down
