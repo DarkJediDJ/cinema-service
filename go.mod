234a03524941d11d6c456b5e8e223ca93d28faf1
DarkJediDJ <xintut@mail.ru>
2022-03-22 01:36:19 +0300

    gg

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/swaggo/http-swagger v1.2.5
 	github.com/swaggo/swag v1.8.0
 	go.uber.org/zap v1.21.0
+	google.golang.org/grpc v1.45.0
+	google.golang.org/protobuf v1.27.1
 )
 
 require (
@@ -28,6 +30,7 @@ require (
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/spec v0.20.4 // indirect
 	github.com/go-openapi/swag v0.21.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -39,6 +42,7 @@ require (
 	golang.org/x/sys v0.0.0-20220224120231-95c6836cb0e7 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.9 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- package/grpc_client.go
+++ package/grpc_client.go
@@ -1,3 +1,54 @@
 package service
 
-cd 
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/darkjedidj/cinema-service/internal"
+	h "github.com/darkjedidj/cinema-service/internal/repository/halls"
+	pb "github.com/darkjedidj/cinema-service/package/proto"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const port = "localhost:50051"
+
+type Client struct {
+	repo *h.Repository
+	log  *zap.Logger
+}
+
+// CreatePDF implements proto.NewTicketGeneratorServer
+func (c *Client) CreatePDF(id int64) (int64, error) {
+	conn, err := grpc.Dial(port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewTicketGeneratorClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	entity, err := c.repo.Retrieve(id)
+	if err != nil {
+		return 0, internal.ErrInternalFailure
+	}
+
+	res, ok := entity.(*h.Resource)
+	if !ok {
+		c.log.Info("Failed to assert movie object.",
+			zap.Bool("ok", ok),
+		)
+
+		return 0, internal.ErrInternalFailure
+	}
+
+	_, err = client.GetTicket(ctx, &pb.TicketRequset{res.Seats})
+	if err != nil {
+		log.Fatalf("could not generate ticket: %v", err)
+	}
+}
