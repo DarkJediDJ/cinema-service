56f0710c7d61668e65c96e44e5039fb55cd33334
DarkJediDJ <xintut@mail.ru>
2022-03-22 20:06:11 +0300

    test(Sessions): added repo and handler tests

---
+++ api/sessions/handler_test.go
@@ -0,0 +1,284 @@
+package sessions
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+
+	"github.com/darkjedidj/cinema-service/internal"
+	movie "github.com/darkjedidj/cinema-service/internal/repository/sessions"
+	"github.com/darkjedidj/cinema-service/test"
+)
+
+func TestCreate(t *testing.T) {
+	testCreateCases := []struct {
+		name           string
+		mockService    *test.MockService
+		body           string
+		expectedStatus int
+	}{
+		{
+			name: "failure: empty body",
+			mockService: &test.MockService{
+				ExpectedResult: &movie.Resource{
+					Hall_id:  4,
+					Movie_id: 2,
+					Schedule: "14:35",
+				},
+			},
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			name: "success",
+			mockService: &test.MockService{
+				ExpectedResult: &movie.Resource{
+					Hall_id:  4,
+					Movie_id: 2,
+					Schedule: "14:35",
+				},
+			},
+			body: `{
+				Hall_id:  4,
+				Movie_id: 2,
+				Schedule: "14:35",
+			},`,
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name: "failure: DB error",
+			mockService: &test.MockService{
+				ExpectedError: internal.ErrInternalFailure,
+			},
+			body: `{
+				Hall_id:  4,
+				Movie_id: 2,
+				Schedule: "14:35",
+			},`,
+			expectedStatus: http.StatusUnprocessableEntity,
+		},
+	}
+	for _, tc := range testCreateCases {
+
+		t.Run(tc.name, func(t *testing.T) {
+
+			logger, err := zap.NewProduction()
+			if err != nil {
+				log.Fatalf("can't initialize zap logger: %v", err)
+			}
+
+			defer func() {
+				if err := logger.Sync(); err != nil {
+					fmt.Println(err)
+				}
+			}()
+
+			w := httptest.NewRecorder()
+
+			r := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(tc.body))
+			r.Header.Set("Content-Type", "application/json")
+
+			(&Handler{s: tc.mockService, log: logger}).Handle(w, r)
+
+			assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
+			assert.Equal(t, tc.expectedStatus, w.Code)
+
+		})
+	}
+}
+
+func TestRetrieve(t *testing.T) {
+	testRetrieveCases := []struct {
+		name           string
+		mockService    *test.MockService
+		id             int64
+		expectedStatus int
+	}{
+		{
+			name: "failure: no rows",
+			mockService: &test.MockService{
+				ExpectedResult: nil,
+			},
+			id:             20,
+			expectedStatus: http.StatusNotFound,
+		},
+		{
+			name: "success",
+			mockService: &test.MockService{
+				ExpectedResult: &movie.Resource{
+					Hall_id:  4,
+					Movie_id: 2,
+					Schedule: "14:35",
+				},
+			},
+			id:             15,
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name: "failure: DB error",
+			mockService: &test.MockService{
+				ExpectedError: internal.ErrInternalFailure,
+			},
+			id:             15,
+			expectedStatus: http.StatusUnprocessableEntity,
+		},
+	}
+	for _, tc := range testRetrieveCases {
+
+		logger, err := zap.NewProduction()
+		if err != nil {
+			log.Fatalf("can't initialize zap logger: %v", err)
+		}
+
+		defer func() {
+			if err := logger.Sync(); err != nil {
+				fmt.Println(err)
+			}
+		}()
+
+		t.Run(tc.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+
+			vars := map[string]string{
+				"id": fmt.Sprintf("%d", tc.id),
+			}
+
+			r := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/%d", tc.id), nil)
+
+			r = mux.SetURLVars(r, vars)
+
+			r.Header.Set("Content-Type", "application/json")
+
+			(&Handler{s: tc.mockService}).HandleID(w, r)
+
+			assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
+			assert.Equal(t, tc.expectedStatus, w.Code)
+
+		})
+	}
+}
+
+func TestRetrieveAll(t *testing.T) {
+	testRetrieveAllCases := []struct {
+		name           string
+		mockService    *test.MockService
+		expectedStatus int
+	}{
+		{
+			name: "failure: no rows",
+			mockService: &test.MockService{
+				ExpectedArray: nil,
+			},
+			expectedStatus: http.StatusNotFound,
+		},
+		{
+			name: "success",
+			mockService: &test.MockService{
+				ExpectedArray: []internal.Identifiable{&movie.Resource{
+					Hall_id:  4,
+					Movie_id: 2,
+					Schedule: "14:35",
+				}},
+			},
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name: "failure: DB error",
+			mockService: &test.MockService{
+				ExpectedError: internal.ErrInternalFailure,
+			},
+			expectedStatus: http.StatusUnprocessableEntity,
+		},
+	}
+	for _, tc := range testRetrieveAllCases {
+
+		logger, err := zap.NewProduction()
+		if err != nil {
+			log.Fatalf("can't initialize zap logger: %v", err)
+		}
+
+		defer func() {
+			if err := logger.Sync(); err != nil {
+				fmt.Println(err)
+			}
+		}()
+
+		t.Run(tc.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+
+			r := httptest.NewRequest(http.MethodGet, "/", nil)
+			r.Header.Set("Content-Type", "application/json")
+
+			(&Handler{s: tc.mockService}).Handle(w, r)
+
+			assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
+			assert.Equal(t, tc.expectedStatus, w.Code)
+
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	testDeleteCases := []struct {
+		name           string
+		mockService    *test.MockService
+		id             int64
+		expectedStatus int
+		prepare        func() *zap.Logger
+	}{
+		{
+			name: "success",
+			mockService: &test.MockService{
+				ExpectedResult: nil,
+			},
+			id:             15,
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name: "failure: DB error",
+			mockService: &test.MockService{
+				ExpectedError: internal.ErrInternalFailure,
+			},
+			id:             15,
+			expectedStatus: http.StatusUnprocessableEntity,
+		},
+	}
+	for _, tc := range testDeleteCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			logger, err := zap.NewProduction()
+			if err != nil {
+				log.Fatalf("can't initialize zap logger: %v", err)
+			}
+
+			defer func() {
+				if err := logger.Sync(); err != nil {
+					fmt.Println(err)
+				}
+			}()
+
+			w := httptest.NewRecorder()
+
+			vars := map[string]string{
+				"id": fmt.Sprintf("%d", tc.id),
+			}
+
+			r := httptest.NewRequest(http.MethodDelete, fmt.Sprintf("/%d", tc.id), nil)
+
+			r = mux.SetURLVars(r, vars)
+
+			r.Header.Set("Content-Type", "application/json")
+
+			(&Handler{s: tc.mockService}).HandleID(w, r)
+
+			assert.Equal(t, tc.expectedStatus, w.Code)
+
+		})
+	}
+}
---
+++ internal/repository/sessions/session_test.go
@@ -0,0 +1,305 @@
+package session
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/darkjedidj/cinema-service/internal"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+)
+
+var session = &Resource{
+	ID:       15,
+	Hall_id:  0,
+	Movie_id: 0,
+	Schedule: "13:25",
+	VIP:      true,
+	Name:     "Matrix",
+}
+
+func NewMock() (*sql.DB, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		log.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	return db, mock
+}
+
+func TestCreate(t *testing.T) {
+	db, mock := NewMock()
+	defer func() {
+		db.Close()
+	}()
+
+	testCreateCases := []struct {
+		name           string
+		expectedError  error
+		expectedResult internal.Identifiable
+		prepare        func(sqlm2 sqlmock.Sqlmock)
+	}{
+		{
+			name:           "failed, database error",
+			expectedError:  internal.ErrInternalFailure,
+			expectedResult: nil,
+			prepare: func(sqlm2 sqlmock.Sqlmock) {
+				sqlm2.ExpectQuery("INSERT INTO sessions (.*)").
+					WillReturnError(internal.ErrInternalFailure)
+			},
+		},
+		{
+			name:           "success",
+			expectedError:  nil,
+			expectedResult: session,
+			prepare: func(sqlm2 sqlmock.Sqlmock) {
+				sqlm2.ExpectQuery("INSERT INTO sessions (.*)").
+					WillReturnRows(sqlm2.
+						NewRows([]string{"id"}).
+						AddRow(session.ID))
+				sqlm2.ExpectQuery("SELECT sessions.id, halls.vip, movies.name, schedule FROM sessions JOIN movies ON sessions.movie_id = movies.id JOIN halls ON sessions.hall_id = halls.id WHERE sessions.id = \\$1").
+					WithArgs(session.ID).
+					WillReturnRows(sqlm2.
+						NewRows([]string{"id", "vip", "name", "schedule"}).
+						AddRow(session.ID, session.VIP, session.Name, session.Schedule))
+			},
+		},
+		{
+			name:           "failed, retrieve error",
+			expectedError:  internal.ErrInternalFailure,
+			expectedResult: nil,
+			prepare: func(sqlm2 sqlmock.Sqlmock) {
+				sqlm2.ExpectQuery("INSERT INTO sessions (.*)").
+					WillReturnError(fmt.Errorf("unable to retrieve Resource"))
+			},
+		},
+	}
+
+	for _, tc := range testCreateCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			logger, err := zap.NewProduction()
+			if err != nil {
+				log.Fatalf("can't initialize zap logger: %v", err)
+			}
+
+			defer func() {
+				if err := logger.Sync(); err != nil {
+					fmt.Println(err)
+				}
+			}()
+
+			repo := &Repository{DB: db, Log: logger}
+
+			tc.prepare(mock)
+			res, err := repo.Create(session)
+
+			assert.Equal(t, tc.expectedResult, res)
+			assert.Equal(t, tc.expectedError, err)
+		})
+	}
+}
+
+func TestRetrieve(t *testing.T) {
+	db, mock := NewMock()
+	defer func() {
+		db.Close()
+	}()
+
+	testRetrieveCases := []struct {
+		name           string
+		expectedError  error
+		expectedResult internal.Identifiable
+		prepare        func(sqlm2 sqlmock.Sqlmock)
+		id             int64
+	}{
+		{
+			name:           "success",
+			expectedError:  nil,
+			expectedResult: session,
+			prepare: func(sqlm2 sqlmock.Sqlmock) {
+				sqlm2.ExpectQuery("SELECT sessions.id, halls.vip, movies.name, schedule FROM sessions JOIN movies ON sessions.movie_id = movies.id JOIN halls ON sessions.hall_id = halls.id WHERE sessions.id = \\$1").
+					WithArgs(session.ID).
+					WillReturnRows(sqlm2.
+						NewRows([]string{"id", "vip", "name", "schedule"}).
+						AddRow(session.ID, session.VIP, session.Name, session.Schedule))
+			},
+			id: int64(session.ID),
+		},
+		{
+			name:           "failed, database error",
+			expectedError:  internal.ErrInternalFailure,
+			expectedResult: nil,
+			prepare: func(sqlm2 sqlmock.Sqlmock) {
+				sqlm2.ExpectQuery("SELECT sessions.id, halls.vip, movies.name, schedule FROM sessions JOIN movies ON sessions.movie_id = movies.id JOIN halls ON sessions.hall_id = halls.id WHERE sessions.id = \\$1").
+					WillReturnError(fmt.Errorf("unable to perform your request, please try again later"))
+			},
+			id: int64(session.ID),
+		},
+		{
+			name:           "failed, sql no rows error",
+			expectedError:  nil,
+			expectedResult: nil,
+			prepare: func(sqlm2 sqlmock.Sqlmock) {
+				sqlm2.ExpectQuery("SELECT sessions.id, halls.vip, movies.name, schedule FROM sessions JOIN movies ON sessions.movie_id = movies.id JOIN halls ON sessions.hall_id = halls.id WHERE sessions.id = \\$1").
+					WillReturnRows(sqlm2.
+						NewRows(nil))
+			},
+			id: 5,
+		},
+	}
+
+	for _, tc := range testRetrieveCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			logger, err := zap.NewProduction()
+			if err != nil {
+				log.Fatalf("can't initialize zap logger: %v", err)
+			}
+
+			defer func() {
+				if err := logger.Sync(); err != nil {
+					fmt.Println(err)
+				}
+			}()
+
+			repo := &Repository{DB: db, Log: logger}
+
+			tc.prepare(mock)
+			res, err := repo.Retrieve(session.ID)
+
+			assert.Equal(t, tc.expectedResult, res)
+			assert.Equal(t, tc.expectedError, err)
+		})
+	}
+}
+
+func TestRetrieveAll(t *testing.T) {
+	db, mock := NewMock()
+	defer func() {
+		db.Close()
+	}()
+
+	testRetrieveAllCases := []struct {
+		name           string
+		expectedError  error
+		expectedResult []internal.Identifiable
+		prepare        func(sqlm2 sqlmock.Sqlmock)
+	}{
+		{
+			name:           "success",
+			expectedError:  nil,
+			expectedResult: []internal.Identifiable{session},
+			prepare: func(sqlm2 sqlmock.Sqlmock) {
+				sqlm2.ExpectQuery("SELECT sessions.id, halls.vip, movies.name, schedule FROM sessions JOIN movies ON sessions.movie_id = movies.id JOIN halls ON sessions.hall_id = halls.id").
+					WillReturnRows(sqlm2.
+						NewRows([]string{"id", "vip", "name", "schedule"}).
+						AddRow(session.ID, session.VIP, session.Name, session.Schedule))
+			},
+		},
+		{
+			name:           "failed, database error",
+			expectedError:  internal.ErrInternalFailure,
+			expectedResult: nil,
+			prepare: func(sqlm2 sqlmock.Sqlmock) {
+				sqlm2.ExpectQuery("SELECT sessions.id, halls.vip, movies.name, schedule FROM sessions JOIN movies ON sessions.movie_id = movies.id JOIN halls ON sessions.hall_id = halls.id").
+					WillReturnError(internal.ErrInternalFailure)
+			},
+		},
+		{
+			name:           "failed, sql no rows error",
+			expectedError:  nil,
+			expectedResult: []internal.Identifiable{},
+			prepare: func(sqlm2 sqlmock.Sqlmock) {
+				sqlm2.ExpectQuery("SELECT sessions.id, halls.vip, movies.name, schedule FROM sessions JOIN movies ON sessions.movie_id = movies.id JOIN halls ON sessions.hall_id = halls.id").
+					WillReturnRows(sqlm2.NewRows([]string{}))
+			},
+		},
+	}
+
+	for _, tc := range testRetrieveAllCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			logger, err := zap.NewProduction()
+			if err != nil {
+				log.Fatalf("can't initialize zap logger: %v", err)
+			}
+
+			defer func() {
+				if err := logger.Sync(); err != nil {
+					fmt.Println(err)
+				}
+			}()
+
+			repo := &Repository{DB: db, Log: logger}
+
+			tc.prepare(mock)
+			res, err := repo.RetrieveAll()
+			assert.Equal(t, tc.expectedResult, res)
+			assert.Equal(t, tc.expectedError, err)
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	db, mock := NewMock()
+	defer func() {
+		db.Close()
+	}()
+
+	testDeleteCases := []struct {
+		name           string
+		expectedError  error
+		expectedResult internal.Identifiable
+		prepare        func(sqlm2 sqlmock.Sqlmock)
+		id             int64
+	}{
+		{
+			name:           "success",
+			expectedError:  nil,
+			expectedResult: session,
+			prepare: func(sqlm2 sqlmock.Sqlmock) {
+				sqlm2.ExpectExec("DELETE FROM sessions WHERE id = \\$1").
+					WithArgs(session.ID).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+			},
+			id: int64(session.ID),
+		},
+		{
+			name:           "failed, database error",
+			expectedError:  internal.ErrInternalFailure,
+			expectedResult: nil,
+			prepare: func(sqlm2 sqlmock.Sqlmock) {
+				sqlm2.ExpectExec("DELETE FROM sessions WHERE id = \\$1").
+					WithArgs(session.ID).
+					WillReturnError(fmt.Errorf("unable to perform your request, please try again later"))
+			},
+			id: int64(session.ID),
+		},
+	}
+
+	for _, tc := range testDeleteCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			logger, err := zap.NewProduction()
+			if err != nil {
+				log.Fatalf("can't initialize zap logger: %v", err)
+			}
+
+			defer func() {
+				if err := logger.Sync(); err != nil {
+					fmt.Println(err)
+				}
+			}()
+
+			repo := &Repository{DB: db, Log: logger}
+
+			tc.prepare(mock)
+			err = repo.Delete(tc.id)
+			assert.Equal(t, tc.expectedError, err)
+		})
+	}
+}
