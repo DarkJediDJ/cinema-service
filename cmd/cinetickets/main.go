820216982a305c868936391279227c3d3db3fb08
Raman Auseyenka <avseenko00@mail.ru>
2022-03-31 19:52:57 +0300

    fix

--- cmd/cinetickets/main.go
+++ cmd/cinetickets/main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"os"
 
-// 	"github.com/joho/godotenv"
+	// 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"go.uber.org/zap"
 
@@ -35,10 +35,10 @@ func main() {
 
 	a := server.App{}
 
-// 	err := godotenv.Load(".env")
-// 	if err != nil {
-// 		log.Fatal("Error loading .env file")
-// 	}
+	// 	err := godotenv.Load(".env")
+	// 	if err != nil {
+	// 		log.Fatal("Error loading .env file")
+	// 	}
 
 	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 
